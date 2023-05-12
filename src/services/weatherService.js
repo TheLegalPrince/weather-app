9dd8f1930f7323d825d482fe236f449000c21a2b
Prince <meprincesr@gmail.com>
2023-05-12 19:17:58 +0530

    reverted API URL

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,13 +1,10 @@
 import { DateTime } from "luxon";
 
 const API_KEY="8ec57a5dc3a2e3f5d5ed909618b0c2b0";
-const BASE_URL={
-    current: "https://api.openweathermap.org/data/2.5",
-    onecall:"https://api.openweathermap.org/data/3.0"
-    };
+const BASE_URL="https://api.openweathermap.org/data";
 
 const getWeatherData=(infoType, searchParams) =>{
-    const url = new URL(`${BASE_URL[infoType]}/${infoType}`);
+    const url = new URL(BASE_URL+ "/" + infoType);
     url.search = new URLSearchParams({...searchParams,appid:API_KEY});
 
     
@@ -44,7 +41,7 @@ const formattedforecastWeather=(data)=>{
     hourly=hourly.slice(1,6).map(d=>{
         return{
             title:formatToLocalTime(d.dt,timezone,'hh:mm a'),
-            temp:d.temp,
+            temp:d.temp.day,
             icon: d.weather[0].icon
         }
     });
@@ -53,10 +50,10 @@ const formattedforecastWeather=(data)=>{
 }
 
 const getFormattedWeatherData=async (searchParams)=>{
-    const formattedCurrentWeather = await getWeatherData("weather", searchParams).then(formatCurrentWeather);
-    const {lat,lon} = formatCurrentWeather;
+    const formattedCurrentWeather=await getFormattedWeatherData('2.5/weather',searchParams).then(formatCurrentWeather)
+    const {lat,lon} = formatCurrentWeather
 
-    const formattedforecastWeather=await getWeatherData('onecall', {lat,lon,exclude:'current,minutely,alerts', units: searchParams.units}).then(formattedforecastWeather);
+    const formattedforecastWeather=await getWeatherData('3.0/onecall', {lat,lon,exclude:'current,minutely,alerts', units: searchParams.units}).then(formattedforecastWeather);
     
     return {...formattedCurrentWeather, ...formattedforecastWeather};
 };
