27e5516b54b304cc0b00bce662cc5e8ccb06a650
Prince <meprincesr@gmail.com>
2023-05-17 12:11:31 +0530

    error fix

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -73,7 +73,7 @@ const getFormattedWeatherData=async (searchParams)=>
     return {...formattedCurrentWeather, ...formattedforecastWeather};
 };
 
-const formatToLocalTime=(secs,zone,format="cccc,dd LLL yyyy' | Local time: 'hh:mm a")=>DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+const formatToLocalTime=(secs,zone,format="cccc, dd LLL yyyy' | Local time: 'hh:mm a")=>DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 const iconUrlFromCode=(code)=>`http://openweathermap.org/img/wn/${code}@2x.png`
 
